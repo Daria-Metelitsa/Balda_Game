a11e7b2216611c1f9750a955258b2214e4ff5ace
Daria-Metelitsa <metel@e-mail.ua>
2013-10-14 14:55:07 +0300

    Add game-letter.html file. Write game-menu.html. Add function letter select. Create array with letter and points. Update other files.

--- js/main.js
+++ js/main.js
@@ -4,6 +4,26 @@
 
 /* анонимная функция*/
 $(function() {
+
+  var array_letter_points = {
+    'A': 1, 'Б': 2, 'В': 1, 'Г': 2,
+    'Д': 1, 'Е': 1, 'Ж': 3, 'З': 3,
+    'И': 1, 'Й': 3, 'К': 1, 'Л': 1,
+    'М': 1, 'Н': 1, 'О': 1, 'П': 1,
+    'Р': 1, 'С': 1, 'Т': 1, 'У': 2,
+    'Ф': 4, 'Х': 3, 'Ц': 4, 'Ч': 3,
+    'Ш': 4, 'Щ': 4, 'Ъ': 5, 'Ы': 3,
+    'Ь': 3, 'Э': 5, 'Ю': 3, 'Я': 2
+  };
+
+  var last_click = null;
+
+  //получить очки за букву
+  function getLetterPoints(letter){
+    return array_letter_points[letter];
+  }
+
+  //переход на страницу игрового процесса и генерация поля произвольного размера
   $(document).on({
     click: function() {
       var field_size = $('input[name="field-size"]:checked').attr('data-field-size');
@@ -14,7 +34,7 @@ $(function() {
         for (var i = 0; i < field_size; i++) {
           html += '<tr>';
           for (var j = 0; j < field_size; j++) {
-            html += '<td id="cell-' + i + '-' + j + '" class="cell cell-' + field_size + '">rere</td>';
+            html += '<td id="cell-' + i + '-' + j + '" class="cell cell-' + field_size + '"> ' + i + '-' + j + ' </td>';
           }
           html += '</tr>';
         }
@@ -43,4 +63,25 @@ $(function() {
 
   $('#to-main').click();
 
+  //переход на форму выбора буквы
+  $(document).on({
+    click: function() {
+      last_click = $(this);
+      $('#main').attr('style', 'display:none;');
+
+      $('body').append('<div class="main"></div>');
+      $('.main').load('/balda/game_letter.html #main');
+    }
+  }, '.cell');
+
+  //переход обратно на форму с игрой (из формы выбора буквы)
+  $(document).on({
+    click: function() {
+      var obj = $(this);
+      last_click.html(obj.find('span').html());
+      last_click.addClass('');
+      $('#main').attr('style', 'display:block;');
+      $('.main').remove();
+    }
+  }, '.letter');
 })
