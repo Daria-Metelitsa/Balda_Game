2957444af8db5ebd3a171e5a66ee5b4dfab31cdd
Daria-Metelitsa <metel@e-mail.ua>
2013-12-08 20:06:54 +0200

    Add protection from zoom and scroll

--- js/main.js
+++ js/main.js
@@ -603,7 +603,12 @@ $(function() {
         }
         return player2.total;
     }
-	
+
+  //защита от склоллинга
+  document.body.addEventListener('touchmove', function(event) {
+      event.preventDefault();
+    }, false);
+
 	//проверка на наличие локального хранилища в браузере
     function isLocalStorageAvailable() {
         try {
